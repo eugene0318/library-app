708a118511a629e4ebd539e93d9ef23c3e6e560c
eugene0318 <nttott@gmail.com>
2023-11-22 01:47:32 +0900

    first commit

---
+++ library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -0,0 +1,11 @@
+package com.group.libraryapp.controller.calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+}
